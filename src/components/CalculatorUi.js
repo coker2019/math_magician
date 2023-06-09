b72f524d4a18d26da249c8d23c201ad59f1a2192
coker2019 <olabisi2k11@gmail.com>
2023-06-09 18:12:55 +0100

    Add calculator components

---
+++ src/components/CalculatorUi.js
@@ -0,0 +1,28 @@
+const CalculatorUi = () => (
+  <>
+    <div className="calculator-container">
+      <div className="calculator-display">0</div>
+      <button type="button" className="button bg-1">AC</button>
+      <button type="button" className="button bg-1">+/-</button>
+      <button type="button" className="button bg-1">%</button>
+      <button type="button" className="button bg-2">÷</button>
+      <button type="button" className="button bg-1">7</button>
+      <button type="button" className="button bg-1">8</button>
+      <button type="button" className="button bg-1">9</button>
+      <button type="button" className="button bg-2">x</button>
+      <button type="button" className="button bg-1">4</button>
+      <button type="button" className="button bg-1">5</button>
+      <button type="button" className="button bg-1">6</button>
+      <button type="button" className="button bg-2">-</button>
+      <button type="button" className="button bg-1">1</button>
+      <button type="button" className="button bg-1">2</button>
+      <button type="button" className="button bg-1">3</button>
+      <button type="button" className="button bg-2">+</button>
+      <button type="button" className="button bg-1 zero-btn">0</button>
+      <button type="button" className="button bg-1">.</button>
+      <button type="button" className="button bg-2">=</button>
+    </div>
+  </>
+);
+
+export default CalculatorUi;
