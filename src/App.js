4c683d47d2c0bffedff68c0f18d15b9b0d2d1f4b
coker2019 <olabisi2k11@gmail.com>
2023-06-15 11:35:47 +0100

    Implemeted web pages functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import Home from './web_pages/Home';
+import Calculator from './web_pages/Calculator';
+import Quote from './web_pages/Quote';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Quote />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </Router>
   );
 }
 
